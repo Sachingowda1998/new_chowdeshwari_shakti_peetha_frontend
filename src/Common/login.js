d72d296547ffd1ff44ae848270c232527a23baea
Sachingowda1998 <parnetstech11@gmail.com>
2025-01-04 18:04:16 +0530

    Changes 7th time 4th January 2025

--- src/Common/login.js
+++ src/Common/login.js
@@ -20,19 +20,8 @@ const Login = () => {
 
             const result = await response.json();
             if (response.ok) {
-                    // Store the userToken in a non-HttpOnly cookie (no security measures)
-    Cookies.set('userToken', result.token, {
-        secure: false,    // Set to false because you're not using HTTPS
-        sameSite: 'None', // For cross-origin requests
-        path: '/'          // Accessible across the entire domain
-    });
-
-    // Optionally, store loginType in a cookie as well
-    Cookies.set('loginType', result.loginType, {
-        secure: false,    // Set to false because you're not using HTTPS
-        sameSite: 'None', // For cross-origin requests
-        path: '/'          // Accessible across the entire domain
-    });
+                // Cookies.set('userToken', result.token);
+                // Cookies.set('loginType', result.loginType);
                 // Save loginType in localStorage (as it's not sensitive)
                 // localStorage.setItem('loginType', result.loginType);
                 Swal.fire('Success', result.message, 'success')
